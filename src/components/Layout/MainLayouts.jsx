82140156d2d8e7d095c6b6a13c72b4a59810bc3d
Adilet <baryktabasovadi@gmail.com>
2023-01-18 20:53:04 +0600

    added some folders

---
+++ src/components/Layout/MainLayouts.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainLayouts = () => {
+  return (
+    <div>
+      <main className="min-h-[70vh]">{children}</main>
+    </div>
+  );
+};
+
+export default MainLayouts;
